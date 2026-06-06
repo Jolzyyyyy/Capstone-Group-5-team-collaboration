cb438a2714dbe56f108bf47b29b5797b617b260a
Julie Anne Calusa <julieannecalusa@gmail.com>
2026-06-07 03:57:32 +0800

    Extract storefront product modal component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,20 +72,7 @@
 
 <x-storefront.cart-drawer />
 
-<div id="productModal" class="product-modal">
-    <div class="product-modal-content">
-        <span class="close-modal" onclick="closeModal()">&times;</span>
-        <h2 id="modalTitle">CHOOSE TYPE</h2>
-        <div class="category-slider">
-            <button class="modal-btn" id="modalPrev" onclick="moveSlide(-1)">&lt;</button>
-            <div class="slider-viewport">
-                <div class="category-track" id="categoryTrack">
-                    </div>
-            </div>
-            <button class="modal-btn" id="modalNext" onclick="moveSlide(1)">&gt;</button>
-        </div>
-    </div>
-</div>
+<x-storefront.product-modal />
 
 <script src="{{ asset('webproj.js') }}?v={{ filemtime(public_path('webproj.js')) }}"></script>
 
